459f1b5baaaf3cd83c7440be2927541b52dab59b
Cheney Mose <1290288968@qq.com>
2022-02-28 13:26:49 +0800

    test 222.2.28

--- src/com/bantanger/sparsearray/Test1248.java
+++ src/com/bantanger/sparsearray/Test1248.java
@@ -4,5 +4,5 @@ package com.bantanger.sparsearray;
  * @author bantanger 半糖
  * @version 1.0
  */
-public class Test123 {
+public class Test1248 {
 }
--- src/com/bantanger/sparsearray/Test14.java
+++
@@ -1,8 +0,0 @@
-package com.bantanger.sparsearray;
-
-/**
- * @author bantanger 半糖
- * @version 1.0
- */
-public class Test14 {
-}
